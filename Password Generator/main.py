93c8c09af4670d97c4fa7907964907cd5df42dc4
Naveen <neonew8984@gmail.com>
2020-11-08 12:10:19 +0530

    password generator initial commit

---
+++ Password Generator/main.py
@@ -0,0 +1,29 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+new_password = ''
+while nr_letters > 0 or nr_numbers>0 or nr_symbols>0:
+  lottery = random.randint(1,3)
+  if lottery == 1 and nr_letters > 0:
+    new_password += random.choice(letters)
+    nr_letters -= 1
+  if lottery == 2 and nr_numbers >0:
+    new_password += random.choice(numbers)
+    nr_numbers -= 1
+  if lottery == 3 and nr_symbols > 0:
+    new_password += random.choice(symbols) 
+    nr_symbols -= 1
+
+    
+
+
+print('Your password is: ' + new_password)
